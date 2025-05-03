2e439fe479135de697c4573e5849a1894dff4de8
Omar Adly <omarelgaafaryy@gmail.com>
2025-05-04 01:49:30 +0300

    Added Insert At Position & Linked_Length

--- new_linked_list.cpp
+++ new_linked_list.cpp
@@ -14,7 +14,7 @@ public:
 
 	void printList() {
 		Node* tmp = head;
-		
+
 		if (head == nullptr)
 		{
 			std::cout << "[Empty]\n";
@@ -28,7 +28,7 @@ public:
 			tmp = tmp->next;
 			i++;
 		}
-		
+
 	}
 
 	void addNode(int n)
@@ -73,19 +73,78 @@ public:
 		head = first_node;
 	}
 
+	Node* find_position(int position)
+	{
+		Node* tmp = new Node();
+		tmp = head;
+		int i = 0;
+
+		while (i != position && tmp->next != nullptr)
+		{
+			tmp = tmp->next;
+			i++;
+
+		}
+		std::cout << tmp->data << std::endl;
+		return tmp;
+	}
+
+	int linked_length()
+	{
+		Node* tmp = head;
+		int i = 0;
+		while (tmp != nullptr)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		return i;
+	}
+
+	void insert_at_position(int pos, int x)
+	{
+		Node* new_node = new Node();
+		new_node->data = x;
+		Node* tmp = head;
+
+		if (pos > linked_length() || pos < 0)
+		{
+			std::cout << "Position out of bounds." << std::endl;
+			return;
+		}
+		
+		int i = 0;
+		while (i != pos - 1 && tmp->next != nullptr)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		new_node->next = tmp->next;
+		tmp->next = new_node;
+
+	}
+
 };
 
 int main()
 {
 	LinkedList newlist;
+	newlist.addNode(0);
+	newlist.addNode(2);
 	newlist.addNode(4);
 	newlist.addNode(6);
 	newlist.addNode(8);
+	newlist.addNode(10);
+	newlist.addNode(12);
+
+
+
 	newlist.printList();
 
 	std::cout << "---------------------------------------\n\n";
-
-	newlist.insert_node_at_beginning(2);
+	
+	newlist.insert_at_position(5, 9);
 	newlist.printList();
+
 
 }
